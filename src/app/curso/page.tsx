cf83716c7a4be4fd862d75f822ccd09c7866cb72
ulysses316 <ulises316@live.com.mx>
2025-03-18 02:08:01 -0600

    [MOD] Hide documentos curso

--- src/app/curso/page.tsx
+++ src/app/curso/page.tsx
@@ -4,11 +4,13 @@ import CourseAccordion from "@/components/couses/CourseAccordion";
 import { authOptions } from "@/utils/authOptions";
 import { getServerSession } from "next-auth";
 import { strapi } from "@/utils/strapi";
-import type { ModulosResponse, StrapiUserApi, StrapiMedia } from "@/utils/types";
+// import type { ModulosResponse, StrapiUserApi, StrapiMedia } from "@/utils/types";
+import type { ModulosResponse, StrapiUserApi } from "@/utils/types";
 import type { AxiosResponse } from "axios";
 import type { Metadata } from "next";
-import { Lock } from "lucide-react";
+// import { Lock } from "lucide-react";
 import qs from "qs";
+// import axios from "axios";
 
 export const metadata: Metadata = {
   title: "Cursos",
@@ -22,29 +24,10 @@ export default async function page() {
   const session = await getServerSession(authOptions);
   let responseUser: AxiosResponse<StrapiUserApi[]> | null = null;
   let response: AxiosResponse<ModulosResponse> | null = null;
-
-  const [requestUser, request] = await Promise.allSettled([
-    strapi.get(`/api/users?filters[documentId][$eq]=${session?.documentId}`),
-    strapi.get("api/modulos-cursos?populate[modulo][populate]=video"),
-    // strapi.get("api/modulos-cursos?populate[modulo][populate][video][populate]=video")
-  ]);
-  if (requestUser.status === "fulfilled") responseUser = requestUser.value;
-  if (request.status === "fulfilled") response = request.value;
-
-  if (!response || !responseUser) return null;
-
-  const responseSorted = response.data.data.sort((a, b) => {
-    const moduloA = parseInt(a.modulo.Modulo.replace(/\D/g, ""), 10) || 0;
-    const moduloB = parseInt(b.modulo.Modulo.replace(/\D/g, ""), 10) || 0;
-
-    return moduloA - moduloB;
-  });
-
-  const userModules = responseUser?.data[0].modulo.split(",").map((modulo) => modulo.trim());
+  // let responseFiles: AxiosResponse<StrapiMedia[]> | null = null;
 
   // Files of the course
-
-  const query = qs.stringify(
+  const queryFile = qs.stringify(
     {
       filters: {
         mime: {
@@ -59,7 +42,31 @@ export default async function page() {
     { encodeValuesOnly: true },
   );
 
-  const responseFiles = await strapi.get(`api/upload/files?${query}`);
+  // const [requestUser, request, requestFiles] = await Promise.allSettled([
+  const [requestUser, request] = await Promise.allSettled([
+    strapi.get(`/api/users?filters[documentId][$eq]=${session?.documentId}`),
+    strapi.get("api/modulos-cursos?populate[modulo][populate]=video"),
+    strapi.get(`api/upload/files?${queryFile}`),
+  ]);
+  if (requestUser.status === "fulfilled") responseUser = requestUser.value;
+  if (request.status === "fulfilled") response = request.value;
+  // if (requestFiles.status === "fulfilled") responseFiles = requestFiles.value;
+
+  if (!response || !responseUser) return null;
+
+  const responseSorted = response.data.data.sort((a, b) => {
+    const moduloA = parseInt(a.modulo.Modulo.replace(/\D/g, ""), 10) || 0;
+    const moduloB = parseInt(b.modulo.Modulo.replace(/\D/g, ""), 10) || 0;
+
+    return moduloA - moduloB;
+  });
+
+  const userModules = responseUser?.data[0].modulo.split(",").map((modulo) => modulo.trim());
+
+  // const getPresignedUrl = async (file_name: string) => {
+  //   const signedUrl = await axios.post("/api/presignedurl", { file_name: file_name });
+  //   console.log(signedUrl);
+  // };
 
   return (
     <section className="flex min-h-[32dvh] flex-col gap-4 px-4 py-12 md:px-12">
@@ -88,23 +95,24 @@ export default async function page() {
           );
         })}
 
-      <details className="group relative cursor-pointer rounded-lg border-2 border-sky-300 px-4 py-2 text-sm">
+      {/* <details className="group relative cursor-pointer rounded-lg border-2 border-sky-300 px-4 py-2 text-sm">
         <summary className="font-railey flex items-center justify-between text-2xl text-black">
           Libros y recursos del curso
           {userModules.length === 0 && <Lock />}
         </summary>
         {userModules.length > 0 && (
           <div className="py-2">
-            {responseFiles.data.map((file: StrapiMedia) => (
-              <div key={file.id}>
-                <a href={file.url} target="_blank" rel="noreferrer" className="mb-2 block text-lg">
-                  {file.name}
-                </a>
-              </div>
-            ))}
+            {responseFiles &&
+              responseFiles.data.map((file: StrapiMedia) => (
+                <div key={file.id}>
+                  <p className="mb-2 block text-lg">
+                    {file.name}
+                  </p>
+                </div>
+              ))}
           </div>
         )}
-      </details>
+      </details> */}
     </section>
   );
 }
